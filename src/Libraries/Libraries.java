cb3c2e57b5d06d91bb832cd6cf1f8c9f4ee29aae
Karanei-Kimutai <168116985+Karanei-Kimutai@users.noreply.github.com>
2025-05-14 17:16:37 +0300

    Creating Custom libraries

---
+++ src/Libraries/Libraries.java
@@ -0,0 +1,31 @@
+package Libraries;
+import sampleLibraryPackage.sampleLibraryClass;
+/*
+Libraries are collections of pre-written classes, interfaces and methods that provide reusable code for common programming tasks
+Libraries offer solutions for tasks like file handling, data structures, networking, graphics, database connections etc
+Types of libraries:Standard libraries and External libraries
+Standard libraries: built-in libraries that come with java eg java.lang, java.util, java.io, java.nio, java.sql, javax.swing, java.time, java.security etc
+External Libraries: custom libraries made by others that can be used to bring in extended functionality
+*/
+/*
+How to create your own library
+If you want to share the library externally:
+Create a new project and write the reusable classes, interfaces and methods
+Package it as a .jar using Artifacts(in IntelliJ)
+Import the .jar file in other projects: Add the library to the project structure of the project you want to use it in
+
+If you want to create a library to use in a specific project:
+Create a package for your library code
+Write the classes, interfaces and methods you want to reuse
+Use the package as a library in other parts of your project
+*/
+
+public class Libraries {
+    public static void displayName(String name){
+        System.out.println(name);
+    }
+    public static void main(String[] args){
+      sampleLibraryClass.displayName("Karanei");
+    }
+
+}
---
+++ src/Librarytest/Test.java
@@ -0,0 +1,10 @@
+package Librarytest;
+import Libraries.Libraries;
+
+
+public class Test {
+    public void randomMethod(String name){
+        Libraries.displayName("Karanei");
+    }
+
+}
